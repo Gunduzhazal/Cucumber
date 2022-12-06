f92e558e46d78bcd4d79e5c2afe21b675391ca2b
Gunduzhazal <gunduzhazal90@gmail.com>
2022-12-06 16:57:21 -0600

    Initial commit

---
+++ src/test/java/CucumberTool/RunnerClass.java
@@ -0,0 +1,12 @@
+package CucumberTool;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "/Users/otheraccount/Intellijj/Cucumber/src/test/java/CucumberTool/Login.feature")
+
+
+public class RunnerClass {
+}
